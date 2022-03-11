06b83e21fdeb0d10b66fdd9daef73f8d7b2f70fd
June Kim <godlyjune@gmail.com>
2022-03-11 16:53:53 +0900

    Change href to collection's home in OpenSea

--- client/components/mainFrame/mainFrameViewNavbar.tsx
+++ client/components/mainFrame/mainFrameViewNavbar.tsx
@@ -9,8 +9,12 @@ const View = () => {
           <i className="fas fa-home"></i>
         </a>
       </Link>
-      <a href="https://testnets.opensea.io/" target="_blank" rel="noreferrer noopener">
-        <img src="/assets/images/logo_opensea_transparent.svg" alt="OpenSea image" />
+      <a
+        href="https://testnets.opensea.io/collection/june-kims-trial-nft"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <img src="/assets/images/logo_opensea_transparent.svg" alt="OpenSea logo" />
       </a>
     </nav>
   );
